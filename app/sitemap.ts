43bc6418c06b1d8768c327bca39eaee256c7f601
Hammad Majid <hammadmajid@proton.me>
2025-03-01 01:36:41 +0500

    fix url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,36 +1,38 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 
 async function getPostSlugs(dir: string) {
-    const entries = await fs.readdir(dir, {
-        recursive: true,
-        withFileTypes: true,
-    });
-    return entries
-        .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
-        .map((entry) => {
-            const relativePath = path.relative(
-                dir,
-                path.join(entry.parentPath, entry.name)
-            );
-            return path.dirname(relativePath);
-        })
-        .map((slug) => slug.replace(/\\/g, '/'));
+  const entries = await fs.readdir(dir, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  return entries
+    .filter((entry) => entry.isFile() && entry.name === "page.mdx")
+    .map((entry) => {
+      const relativePath = path.relative(
+        dir,
+        path.join(entry.parentPath, entry.name),
+      );
+      return path.dirname(relativePath);
+    })
+    .map((slug) => slug.replace(/\\/g, "/"));
 }
 
 export default async function sitemap() {
-    const notesDirectory = path.join(process.cwd(), 'app', 'post');
-    const slugs = await getPostSlugs(notesDirectory);
+  const notesDirectory = path.join(process.cwd(), "app", "post");
+  const slugs = await getPostSlugs(notesDirectory);
 
-    const notes = slugs.map((slug) => ({
-        url: `https://hammadmajid.me/me/${slug}`,
-        lastModified: new Date().toISOString(),
-    }));
+  const notes = slugs.map((slug) => ({
+    url: `https://bine.codes/me/${slug}`,
+    lastModified: new Date().toISOString(),
+  }));
 
-    const routes = ['', '/links', '/projects', '/contact', '/meta', 'about'].map((route) => ({
-        url: `https://hammadmajid.me${route}`,
-        lastModified: new Date().toISOString(),
-    }));
+  const routes = ["", "/links", "/projects", "/contact", "/meta", "/about"].map(
+    (route) => ({
+      url: `https://bine.codes${route}`,
+      lastModified: new Date().toISOString(),
+    }),
+  );
 
-    return [...routes, ...notes];
+  return [...routes, ...notes];
 }
