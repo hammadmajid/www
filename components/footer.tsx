87a8bcae63d46f236da05c0dd4a1258f87017393
Hammad Majid <hammadmajid@proton.me>
2025-03-02 01:12:00 +0500

    update colors (again)

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export async function Footer() {
 	return (
-		<footer className="p-4 pb-10 text-center border-t-2">
+		<footer className="p-4 pb-10 text-center border-t-2 border-secondary">
 			<div className="container mx-auto">
 				<p className="text-sm">Unlicensed by Hammad Majid.</p>
 			</div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "@/components/link";
 export function Navbar() {
 	return (
 		<>
-			<header className="p-4 border-b-2 border-light-border text-center space-y-2">
+			<header className="p-4 border-b-2 border-secondary text-center space-y-2">
 				<div className="w-full">
 					<Link href="/">bine</Link>
 				</div>
