164dc154af6833a7f7dc7d5fdf65d66016329d6d
Hammad Majid <hammadmajid@proton.me>
2025-03-02 00:20:06 +0500

    create custom link component

---
+++ components/link.tsx
@@ -0,0 +1,22 @@
+import { Link as NextViewTransitionLink } from "next-view-transitions";
+
+interface LinkProps {
+  href: string;
+  children: React.ReactNode;
+  external?: boolean;
+}
+
+export default function Link({ href, children, external = false }: LinkProps) {
+  const target = external ? "_blank" : "_self";
+  const rel = external ? "noopener noreferrer" : undefined;
+
+  return external ? (
+    <a href={href} target={target} rel={rel}>
+      {children}
+    </a>
+  ) : (
+    <NextViewTransitionLink href={href} rel={rel}>
+      {children}
+    </NextViewTransitionLink>
+  );
+}
