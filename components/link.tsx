4f6a8fd59637f2d6fe4010df3f850d775d0f302a
Hammad Majid <hammadmajid@proton.me>
2025-03-02 00:46:18 +0500

    update link style and format

--- components/link.tsx
+++ components/link.tsx
@@ -1,22 +1,31 @@
 import { Link as NextViewTransitionLink } from "next-view-transitions";
 
 interface LinkProps {
-  href: string;
-  children: React.ReactNode;
-  external?: boolean;
+	href: string;
+	children: React.ReactNode;
+	external?: boolean;
 }
 
 export default function Link({ href, children, external = false }: LinkProps) {
-  const target = external ? "_blank" : "_self";
-  const rel = external ? "noopener noreferrer" : undefined;
+	const target = external ? "_blank" : "_self";
+	const rel = external ? "noopener noreferrer" : undefined;
 
-  return external ? (
-    <a href={href} target={target} rel={rel}>
-      {children}
-    </a>
-  ) : (
-    <NextViewTransitionLink href={href} rel={rel}>
-      {children}
-    </NextViewTransitionLink>
-  );
+	return external ? (
+		<a
+			href={href}
+			target={target}
+			rel={rel}
+			className="text-primary hover:text-primary-hover"
+		>
+			{children}
+		</a>
+	) : (
+		<NextViewTransitionLink
+			href={href}
+			rel={rel}
+			className="text-primary hover:text-primary-hover"
+		>
+			{children}
+		</NextViewTransitionLink>
+	);
 }
