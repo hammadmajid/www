4af5dcdbb101157bf667ec3a109c8c2d608de50e
Hammad Majid <hammadmajid@proton.me>
2025-03-02 00:19:40 +0500

    delete loading.tsx

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <main id="main-content" className="h-screen px-4 grid place-items-center">
-      <div className="text-center space-y-1 md:flex md:flex-row gap-5">
-        <h1 className="text-lg">Loading...</h1>
-      </div>
-    </main>
-  );
-}
