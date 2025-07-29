23406f49dab626f3de2948325e4e40c798082a3b
Hammad Majid <hammadmajid@proton.me>
2025-07-29 17:24:08 +0500

    Add consistant stying on input components

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,8 +7,8 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "placeholder:text-muted-foreground focus-visible:border-white/50 focus-visible:ring-white/30 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-white/10 text-card-foreground flex field-sizing-content min-h-16 w-full rounded-4xl border border-white/30 backdrop-blur-xl supports-[backdrop-filter]:bg-white/5 shadow-2xl shadow-black/20 relative overflow-hidden px-4 py-3 text-base transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "before:absolute before:inset-0 before:bg-gradient-to-br before:from-white/25 before:via-white/5 before:to-transparent before:pointer-events-none before:rounded-4xl",
+        "placeholder:text-muted-foreground focus-visible:border-accent-foreground/40 focus-visible:ring-accent-foreground/20 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-card/20 text-card-foreground flex field-sizing-content min-h-16 w-full rounded-4xl border border-accent-foreground/20 backdrop-blur-md supports-[backdrop-filter]:bg-card/10 shadow-xl shadow-black/10 relative overflow-hidden px-4 py-3 text-base transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "before:absolute before:inset-0 before:bg-gradient-to-br before:from-accent-foreground/10 before:via-transparent before:to-transparent before:pointer-events-none before:rounded-4xl",
         "after:absolute after:inset-[1px] after:rounded-4xl after:bg-gradient-to-br after:from-white/15 after:via-transparent after:to-transparent after:pointer-events-none",
         className
       )}
