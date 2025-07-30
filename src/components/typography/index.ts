0379c886231efbbde8696f77fc50996028e2ea8b
Hammad Majid <hammadmajid@proton.me>
2025-07-30 17:54:35 +0500

    Introduce typography components in seprate file for better usage

---
+++ src/components/typography/index.ts
@@ -0,0 +1,12 @@
+export { default as TypographyH1 } from './typography-h1.astro';
+export { default as TypographyH2 } from './typography-h2.astro';
+export { default as TypographyH3 } from './typography-h3.astro';
+export { default as TypographyH4 } from './typography-h4.astro';
+export { default as TypographyP } from './typography-p.astro';
+export { default as TypographyBlockquote } from './typography-blockquote.astro';
+export { default as TypographyList } from './typography-list.astro';
+export { default as TypographyInlineCode } from './typography-inline-code.astro';
+export { default as TypographyLead } from './typography-lead.astro';
+export { default as TypographyLarge } from './typography-large.astro';
+export { default as TypographySmall } from './typography-small.astro';
+export { default as TypographyMuted } from './typography-muted.astro';
